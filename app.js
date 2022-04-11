6d9ccc4f2cc8b428b90bd3bcb33660c155d4c930
Konsta Nenonen <konstanenonen@protonmail.com>
2022-04-12 00:18:01 +0300

    Started to figure out the playe factory, gameboard module and displayController module

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 "use strict";
+
+const Player = (name) => {
+  const _name = name;
+  
+  const getName = () => {
+    return _name;
+  }
+
+  return {
+    getName
+  }
+}
+
+const gameboard = (() => {
+  const _gameboardArray = ["x", null, null, null, "0", "0", "X", "0", "0"];
+
+  const getBoardState = () => {
+    return _gameboardArray;
+  }
+
+  return {
+    getBoardState
+  }
+})();
+
+const displayController = (() => {
+  const displayBoard = () => {
+    gameboard.getBoardState().forEach(item => {
+      const boardTile = document.createElement("p");
+      boardTile.innerText = item;
+      document.querySelector(".gameboard").appendChild(boardTile);
+    });
+  }
+
+  return {
+    displayBoard
+  }
+})();
