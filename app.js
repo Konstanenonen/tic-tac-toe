9c4e7b602716a01b3263edad76d109a3bbe8a301
Konsta Nenonen <konstanenonen@protonmail.com>
2022-04-12 00:58:26 +0300

    Got the basic functionality to work

--- app.js
+++ app.js
@@ -1,39 +1,82 @@
 "use strict";
 
-const Player = (name) => {
+const Player = (name, marker) => {
   const _name = name;
-  
+  const _marker = marker;
+
   const getName = () => {
     return _name;
   }
 
+  const getMarker = () => {
+    return _marker;
+  }
+
   return {
-    getName
+    getName,
+    getMarker
   }
 }
 
+const player1 = Player("Player1", "X");
+const player2 = Player("Player2", "O");
+
 const gameboard = (() => {
-  const _gameboardArray = ["x", null, null, null, "0", "0", "X", "0", "0"];
+  let _gameboardArray = [null, null, null, null, null, null, null, null, null];
 
   const getBoardState = () => {
     return _gameboardArray;
   }
 
+  const updateBoard = (marker, index) => {
+    if (_gameboardArray[index] !== null) {
+      return;
+    }
+
+    _gameboardArray[index] = marker;
+
+    getBoardState();
+    
+    displayController.displayBoard();
+
+    displayController.changeTurn();
+  }
+
   return {
+    updateBoard,
     getBoardState
   }
 })();
 
 const displayController = (() => {
+
+  let _playerTurn = player1;
+
+  const changeTurn = () => {
+    if (_playerTurn === player1) {
+      _playerTurn = player2;
+    } else {
+      _playerTurn = player1;
+    }
+  }
+
   const displayBoard = () => {
-    gameboard.getBoardState().forEach(item => {
-      const boardTile = document.createElement("p");
+    const boardContainer = document.querySelector(".gameboard");
+    boardContainer.innerHTML = "";
+    gameboard.getBoardState().forEach((item, index) => {
+      const boardTile = document.createElement("div");
       boardTile.innerText = item;
-      document.querySelector(".gameboard").appendChild(boardTile);
+      boardTile.addEventListener("click", () => {
+        gameboard.updateBoard(_playerTurn.getMarker(), index);
+      })
+      boardContainer.appendChild(boardTile);
     });
   }
 
   return {
-    displayBoard
+    displayBoard,
+    changeTurn
   }
 })();
+
+displayController.displayBoard();
