c4ece9ea64e15b3169aee8458228160722743d98
Konsta Nenonen <konstanenonen@protonmail.com>
2022-04-16 01:37:21 +0300

    Started to refactor the code. Trying to make it more clever where each method should be in.

--- app.js
+++ app.js
@@ -34,51 +34,57 @@ const gameboard = (() => {
     if (_gameboardArray[index] !== null) {
       return;
     }
-
     _gameboardArray[index] = marker;
-
-    getBoardState();
-
-    displayController.displayBoard();
-
-    displayController.changeTurn();
-  }
-
-  return {
-    updateBoard,
-    getBoardState
-  }
-})();
-
-// Module to display the game state
-const displayController = (() => {
-  let _playerTurn = player1;
-
-  const changeTurn = () => {
-    if (_playerTurn === player1) {
-      _playerTurn = player2;
-    } else {
-      _playerTurn = player1;
-    }
   }
 
   const displayBoard = () => {
     const boardContainer = document.querySelector(".gameboard");
     boardContainer.innerHTML = "";
-    gameboard.getBoardState().forEach((item, index) => {
+    gameboard.getBoardState().forEach((item) => {
       const boardTile = document.createElement("div");
+      boardTile.classList.add("tile");
       boardTile.innerText = item;
-      boardTile.addEventListener("click", () => {
-        gameboard.updateBoard(_playerTurn.getMarker(), index);
-      })
       boardContainer.appendChild(boardTile);
     });
   }
 
+  const addEventListeners = (player) => {
+    const tiles = document.getElementsByClassName("tile");
+    const tilesArray = Array.from(tiles);
+    tilesArray.forEach((tile, index) => {
+      tile.addEventListener("click", () => {
+        updateBoard(player.getMarker(), index);
+        game.changeTurn();
+      });
+    })
+  }
+
   return {
+    updateBoard,
+    getBoardState,
     displayBoard,
+    addEventListeners
+  }
+})();
+
+// Module to display the game state
+const game = (() => {
+  let _currentPlayer = player1;
+
+  const changeTurn = () => {
+    if (_currentPlayer === player1) {
+      _currentPlayer = player2;
+    } else {
+      _currentPlayer = player1;
+    }
+
+    gameboard.displayBoard();
+    gameboard.addEventListeners(_currentPlayer);
+  }
+
+  return {
     changeTurn
   }
 })();
 
-displayController.displayBoard();
+game.changeTurn();
